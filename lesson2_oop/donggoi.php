fb81ad1e9501d368009cd5ae1800348d928aba70
thienth32 <thienth32@gmail.com>
2019-01-11 08:24:25 +0700

    update mvc

--- lesson2_oop/donggoi.php
+++ lesson2_oop/donggoi.php
@@ -1,37 +1,48 @@
 <?php
-class LopCha{
-    public $ten;
-    protected $tuoi;
-    private $quequan;
+// class LopCha{
+//     public $ten;
+//     protected $tuoi;
+//     private $quequan;
 
     
+// }
+
+// class LopCon extends LopCha{
+//     public function setQueQuan($val){
+//         $this->quequan = $val;
+//     }
+
+//     public function getQueQuan(){
+//         return $this->quequan;
+//     }
+
+//     public function setTuoi($val){
+//         $this->tuoi = $val;
+//     }
+//     public function getTuoi(){
+//         return $this->tuoi;
+//     }
+// }
+
+// $cha = new LopCon();
+// // $cha->setQueQuan('Hanoi');
+// // echo $cha->getQueQuan();
+// // echo "<br>";
+// $cha->setTuoi(50);
+// echo $cha->getTuoi();
+// var_dump($cha);
+class Person{
+	function __construct(){
+		echo "new star is born";
+	}
 }
-
-class LopCon extends LopCha{
-    public function setQueQuan($val){
-        $this->quequan = $val;
-    }
-
-    public function getQueQuan(){
-        return $this->quequan;
-    }
-
-    public function setTuoi($val){
-        $this->tuoi = $val;
-    }
-    public function getTuoi(){
-        return $this->tuoi;
-    }
+class Women extends Person{
+	function gotMarried(){
+		echo "Call me Mrs. ";
+	}
 }
-
-$cha = new LopCon();
-// $cha->setQueQuan('Hanoi');
-// echo $cha->getQueQuan();
-// echo "<br>";
-$cha->setTuoi(50);
-echo $cha->getTuoi();
-var_dump($cha);
-
+$jean = new Women();
+echo $jean->gotMarried();
 
 
 
