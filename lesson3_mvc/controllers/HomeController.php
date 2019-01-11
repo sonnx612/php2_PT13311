a36e4369eaae6efaaa236dcbc3fbbb253a5601cc
thienth32 <thienth32@gmail.com>
2019-01-11 08:53:56 +0700

    update

---
+++ lesson3_mvc/controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+class HomeController{
+    public function index(){
+        echo "HomeController->index";
+    }
+
+    public function danhMuc(){
+        echo "HomeController->danhmuc";
+    }
+}
+
+?>
--- lesson3_mvc/index.php
+++ lesson3_mvc/index.php
@@ -1,7 +1,20 @@
 <?php
 $url = isset($_GET['url']) == true ? $_GET['url'] : "/";
 
-echo $url;
+switch($url){
+    case "/":
+        require_once "./controllers/HomeController.php";
+        $ctr = new HomeController();
+        $ctr->index();
+        break;
+    case "danh-muc":
+        require_once "./controllers/HomeController.php";
+        $ctr = new HomeController();
+        $ctr->danhMuc();
+        break;
+    default:
+        echo "404 not found!";
+}
 
 
 ?>
