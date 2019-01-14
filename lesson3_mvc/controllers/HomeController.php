57bd053ba15b268017b650c548b4c79cb459083e
thienth32 <thienth32@gmail.com>
2019-01-14 08:43:10 +0700

    update raw query function

--- lesson3_mvc/controllers/HomeController.php
+++ lesson3_mvc/controllers/HomeController.php
@@ -3,7 +3,13 @@ require_once "./models/Product.php";
 require_once "./models/Category.php";
 class HomeController{
     public function index(){
-        $product = Product::where('id', '<', "10")->get();
+        $product = Product::rawQuery("select 
+                                                p.*,
+                                                c.cate_name as cate_name
+                                    from products p
+                                    join categories c
+                                        on p.cate_id = c.id
+                                    ");
         echo "<pre>";
         var_dump($product);die;
     }
--- lesson3_mvc/models/BaseModel.php
+++ lesson3_mvc/models/BaseModel.php
@@ -14,8 +14,39 @@ class BaseModel
         return $model;
     }
 
+    public static function all(){
+        $model = new static();
+        $model->queryBuilder = "select * from " . $model->table;
+        return $model->get();
+    }
+
+    public function andWhere($col, $operation, $val){
+        $this->queryBuilder .= " and $col $operation $val";
+        return $this;
+    }
+
+    public function orWhere($col, $operation, $val){
+        $this->queryBuilder .= " or $col $operation $val";
+        return $this;
+    }
+
+    public function delete(){
+        $this->queryBuilder = "delete " . $this->table 
+                                . " where id = " . $this->id;
+        $stmt = $this->connect->prepare($this->queryBuilder);
+        $stmt->execute();
+        return true;
+    }
+
     public function get(){
         $stmt = $this->connect->prepare($this->queryBuilder);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public static function rawQuery($sqlQuery){
+        $model = new static();
+        $stmt = $model->connect->prepare($sqlQuery);
         $stmt->execute();
         return $stmt->fetchAll();
     }
