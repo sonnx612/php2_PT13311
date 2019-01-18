1c1ffd2f0ec56b55e9cdbbe35c3463f192baa6c9
thienth32 <thienth32@gmail.com>
2019-01-18 08:49:12 +0700

    update

--- lesson3_mvc/controllers/ProductController.php
+++ lesson3_mvc/controllers/ProductController.php
@@ -18,27 +18,34 @@ class ProductController
 
     public function saveaAdd(){
         $model = new Product();
+        // biến chứa các tên cột trong câu sql
         $cols = " ";
+        // biến chứa các giá trị tương ứng với cột ở trên
         $vals = " ";
         $img = $_FILES['image'];
         $_POST['image'] = null;
+        // lưu ảnh
         if($img['size'] > 0){
             $filename = "public/images/products/" . uniqid() . "-" . $img['name'];
             move_uploaded_file($img['tmp_name'], $filename);
             $_POST['image'] = $filename;
         }
 
+        // sinh giá trị cho các cột trong câu sql
         foreach($_POST as $key => $val){
             $cols .= " " . $key. ",";
             $vals .= " '" . $val. "',";
         }
+        // loại bỏ dấu "," ở cuối các chuỗi
         $cols = rtrim($cols, ',');
         $vals = rtrim($vals, ',');
-        
+
+        // sinh ra câu lệnh sql
         $sqlQuery = "insert into " . $model->table 
                                  . " ($cols) values ( $vals)";
-        // var_dump($sqlQuery);die;
+        // thực thi câu lệnh đc sinh ra ở trên
         Product::rawQuery($sqlQuery);
+        // điều hướng về trang chủ
         header('location: ./');
     }
 }
